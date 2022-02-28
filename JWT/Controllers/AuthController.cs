901991d80ecb68cc73a5dae9fbd3fb2ccc4b6073
Ahmed Yousef <lajjayousef66@gmail.com>
2022-03-01 01:52:06 +0200

    Secure the api with Authorize Attribute

--- JWT/Controllers/AuthController.cs
+++ JWT/Controllers/AuthController.cs
@@ -1,10 +1,12 @@
 ﻿using JWT.Models;
 using JWT.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JWT.Controllers
 {
+    //[Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class AuthController : ControllerBase
